3eca25bfa65d99789ced2ffacaa0b46b0240c732
areeba1309 <109980203+areeba1309@users.noreply.github.com>
2025-10-03 00:21:21 +0300

    Create [id].js

---
+++ apps/web/pages/product/[id].js
@@ -0,0 +1,8 @@
+export default function ProductPage({ id }) {
+  return (
+    <div className="container">
+      <h1>Product {id}</h1>
+      {/* Placeholder product details */}
+    </div>
+  );
+}
